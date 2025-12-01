eab23c1dca482979f05bf136541022bdcf6f1c4c
GABRIEL SOUSA RODRIGUES DE MELO <25203630@dsn127m1247443.cfp127aluno.local>
2025-12-01 09:37:56 -0300

    Criando as classes JAVA

--- src/br/dev/gabriel/leitura_escrita/LeituraEscritaApp.java
+++ src/br/dev/gabriel/leitura_escrita/LeituraEscritaApp.java
@@ -1,4 +1,8 @@
 package br.dev.gabriel.leitura_escrita;
 
 public class LeituraEscritaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Leitura escrita app");
+    }
 }
--- src/br/dev/gabriel/leitura_escrita/model/Contato.java
+++ src/br/dev/gabriel/leitura_escrita/model/Contato.java
@@ -1,4 +1,9 @@
 package br.dev.gabriel.leitura_escrita.model;
 
 public class Contato {
+    public String id;
+    public String nome;
+    public String email;
+    public String telefone;
+
 }
